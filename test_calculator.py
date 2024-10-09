06a1cd99cd436efa1c3733b3489783bf4cdff681
Molnar Karoly <molnar.karoly@diak.szbi-pg.hu>
2024-10-09 11:36:04 +0200

    all function

--- test_calculator.py
+++ test_calculator.py
@@ -8,7 +8,17 @@ def test_sum():
     
 def test_sub():
     assert sub(2, 1) == 1
-    assert sub(1, -2) == -3
+    assert sub(1, -2) == 3
     assert sub(0, 0) == 0
+    
+def test_mult():
+    assert mult(0, 0) == 0
+    assert mult(2, 1) == 2
+    assert mult(3, 3) == 9
+    
+def test_div():
+    assert div(8, 8) == 1
+    assert div(9, 3) == 3
+    assert div(10, 5) == 2
     
     
